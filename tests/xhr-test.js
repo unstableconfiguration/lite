b4c77265ddadf44f6abc3f93a9c69bedfdba9d5e
muffins <mluper@protonmail.com>
2020-11-26 13:49:24 -0500

    xhr get tests done

--- tests/xhr-test.js
+++ tests/xhr-test.js
@@ -6,40 +6,48 @@ export let XHRTest = function() {
     describe('XHR tests', function() { 
         describe('xhr.get() tests', function() { 
             it('should fetch text data from an external resource', function(done) { 
-                xhr.get('../tests/xhr-test/xhr-test.txt')
-                    .then(r => {
-                        assert(r == 'test');
+                let request = xhr.get('../tests/xhr-test/xhr-test.txt', {
+                    load : function(r) {
+                        assert(request.response == 'test');
                         done();
-                    });
+                    }
+                });
+                request.send();
             });
 
-            // fetch script or alternative data type?
-
-            it('should return an unsent XMLHttpRequest object');
+            it('should return an unsent XMLHttpRequest object', function(done) { 
+                let request = xhr.get('../does/not/really/matter');
+                assert(request instanceof XMLHttpRequest);
+                setTimeout(()=>{
+                    assert(request.readyState == 1);
+                    done();
+                }, 5);
+            });
 
             it('should allow for setting of XMLHttpRequest events', function(done) {
                 let request = xhr.get('../tests/xhr-test/xhr-test.txt', { 
                     load : function() { 
-                        console.log(request.response);
-                        done();
-                    }, 
-                    catch : function() { 
-                        console.log('you done fucked up');
                         done();
                     }
                 });
-                console.log(request);
                 request.send();
             });
 
-            it('should return a "thennable" interface');
+            it('should return a "thennable" interface', function() { 
+                let request = xhr.get('../xyz', { 
+                    load : function() {},
+                    error : function() {}
+                });
+                assert(typeof(request.then) == 'function');
+                assert(typeof(request.catch) == 'function');
+            });
 
-            it('should send when .then() is called')
+            it('should send when .then() is called', function(done) {
+                xhr.get('../tests/xhr-test/xhr-test.txt')
+                    .then(r => { done(); });
+            });
 
         });
-        
-        it('should do stuff')
-
     });
 
 }
