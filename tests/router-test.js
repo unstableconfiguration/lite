62adff623784fccd08bd91c31cdde5321c481674
muffins <mluper@protonmail.com>
2020-11-27 11:02:03 -0500

    adding empty router tests

---
+++ tests/router-test.js
@@ -0,0 +1,15 @@
+import { Router } from '../router.js';
+
+export let RouterTest = function() {
+    describe('Router Tests', function() { 
+        it('should trigger custom onHashChange event when window hash changes');
+
+        it('should return path value if location hash matches path pattern');
+
+        it('should parse url parameters into object when getURLParams is called');
+
+        it('should convert a path string to a regex when getHashRegex is called');
+
+
+    });
+}
