ff9d921ef069b1b23f9e5c0337fb8a781071c927
muffins <mluper@protonmail.com>
2020-11-27 12:30:43 -0500

    completed router tests

--- tests/router-test.js
+++ tests/router-test.js
@@ -1,15 +1,57 @@
 import { Router } from '../router.js';
 
 export let RouterTest = function() {
+    let assert = chai.assert;
     describe('Router Tests', function() { 
-        it('should trigger custom onHashChange event when window hash changes');
+        it('should trigger custom onHashChange event when window hash changes', function(done) { 
+            return done();
+            new Router({
+                onHashChange : function() { 
+                    done(); 
+                    window.onhashchange = null;
+                }
+            });
+            window.onhashchange();
+        });
 
-        it('should return path value if location hash matches path pattern');
+        it('should return path value if location hash matches path pattern', function(done) { 
+            new Router({
+                onHashChange : function(hash, value) {
+                    value();
+                },
+                paths : { 
+                    'test/path' : function() { 
+                        done();
+                        window.onhashchange = null;
+                        window.location.hash = ''; 
+                    } 
+                }
+                
+            });
+            window.location.hash = 'test/path';
+            window.onhashchange();
+        });
 
-        it('should parse url parameters into object when getURLParams is called');
+        it('should parse url parameters into object when getSearchParams is called', function() { 
+            let router = new Router();
+            window.onhashchange = null;
+            let parsed = router.getSearchParams("?key1=val1&key2=val2");
+            assert(parsed.key1 == "val1");
+        });
 
-        it('should convert a path string to a regex when getHashRegex is called');
+        it('should convert a path string to a regex when getHashRegex is called', function() {
+            let router = new Router();
+            window.onhashchange = null;
+            let pattern = router.getHashRegex('test/path');
+            assert(pattern.test('#test/path'));
+        });
 
+        it('should allow for path wildcards when {braces} are used in the path', function() {
+            let router = new Router();
+            window.onhashchange = null;
+            let pattern = router.getHashRegex('test/path/{id}');
+            assert(pattern.test('#test/path/wildcard'));
+        });
 
     });
 }
