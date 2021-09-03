02c4a30b48d91fba922e6eeded503a7b0733b132
oldmanmuffins <mluper.official@protonmail.com>
2021-09-02 20:16:15 -0400

    exporting Lite type

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { router } from './app/router.js'
-import { lite } from './app/lite.js'
+import { lite, Lite } from './app/lite.js'
 import { xhr } from './app/xhr.js'
 
-export { lite, router, xhr }
+export { Lite, lite, router, xhr }
