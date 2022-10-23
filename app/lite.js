ef519eb84c2f40627e87490757a11d7759762033
oldmanmuffins <mluper@protonmail.com>
2022-10-23 10:43:31 -0400

    simplifying and converting lite.js

--- app/lite.js
+++ app/lite.js
@@ -1,130 +1,47 @@
-export let Lite = function(args = {}) {
-    let _lite = this;
-
-    _lite.__container = null;
-    Object.defineProperty(_lite, "container", {
-        get : () => _lite.__container,
-        set : (value) => { 
-            if(typeof(value) === 'string') { value = document.getElementById(value); }
-            if(!(value instanceof HTMLElement)) { throw new Error('Cannot find HTMLElement: ' + value); }
-            _lite.__container = value;
-
-            _lite.__bindContent(); 
-            _lite.__bindData();
-        }
-    });
-
-    _lite.__content = null;
-    Object.defineProperty(_lite, 'content', {
-        get : () => _lite.__content,
-        set : (value) => {
-            if(typeof(value) !== 'string') { return; }
-            _lite.__content = value;
-
-            _lite.__bindContent();
-            _lite.__bindData();
-        }
-    });
-
-    _lite.__data = null;
-    Object.defineProperty(_lite, 'data', {
-        get : () => _lite.__data,
-        set : (value) => { 
-            _lite.__data = value; 
-
-            _lite.__bindData();
-        }
-    });
-
-    /* content binding
-        Clears container and populates with content 
+export class Lite {
+    /* Container: 
+        Can be set as an id for an html element
+        Can be set as a query selector for an html element
+        Can be set as a reference to an html element
     */
-    _lite.__bindContent = function() {
-        if(!_lite.__container || !_lite.__content) { return; }
-        while(_lite.container.firstChild) {
-            _lite.container.removeChild(_lite.container.firstChild);
-        }
-        _lite.container.insertAdjacentHTML('afterbegin', _lite.content);
-        _lite.onContentBound();
-    }
-
-    /* data binding
-        If content is bound: finds any elements with data-field attributes and populates them with 
-        values from data. Matches values on data-field=property or id=property.
-        <span data-field="propertyA"></span>
-        <textbox id="propertyB" data-field></textbox>
+    container = '';
+    /* Content
+        String value to set as the container's innerHTML
     */
-    _lite.__bindData = function() {
-        if(!_lite.__container || !_lite.__content || !_lite.__data) { return; }
+    content = '';
+
+    constructor(options = {}) { 
+        Object.assign(this, options);
         
-        _lite.container.querySelectorAll('[data-field]')
-            .forEach((el) => {
-                let prop = el.getAttribute('data-field') || el.id;
-                let val = prop.split('.').reduce((acc, p) => { return acc[p]; }, _lite.__data)
-                if(typeof(el.value) !== 'undefined') { el.value = val; }
-                else { el.innerHTML = val; }
-            });
-        _lite.onDataBound();
-    }
-
-    _lite.loadStyleSheet = function(uri) {
-        let css = document.createElement('link');
-        css.rel = 'stylesheet';
-        css.type = 'text/css';
-        css.href = uri;
-       
-        let links = document.getElementsByTagName('link');
-        let hasLink = Array.from(links).some((link) => { 
-            return link.href == css.href;
-        });
-        if(hasLink) { return; }
-  
-        let head = document.getElementsByTagName('head')[0];
-        head.appendChild(css);
-        return css;
-    }
-
-    _lite.loadScript = function(uri) {
-        let script = document.createElement('script');
-        script.src = uri;
-
-        let scripts = document.getElementsByTagName('script');
-        let hasScript = Array.from(scripts).some((s) => {
-            return s.src == script.src;
-        });
-        if(hasScript) { return; }
-
-        let head = document.getElementsByTagName('head')[0];
-        head.appendChild(script);
-        return script;
-    }
-
-    _lite.initialize = function(args) { }
-    _lite.onContentBound = function() { }
-    _lite.onDataBound = function() { }
+        this.container = this.#getContainer();
+        if(!this.container instanceof HTMLElement)
+            throw `could not parse container to html element. value is ${this.container}`;
     
-    /* extend 
-        Returns a 'new' constructor. Both .extend() and new() accept 
-        arguments that will be combined together during initialization. 
-
-    */
-    _lite.extend = function(args = {}){
-        return function(more_args = {}) { 
-            for(let k in more_args)
-                args[k] = more_args[k];
-            
-            Lite.call(this, args);
-            return this;
-        }
+        this.#appendContent();
+        this.onContentBound();
     }
-    /* When lite or any derived class is instantiated, the args 
-    can add to it or override its defaults. */
-    for(let a in args) { this[a] = args[a]; }
 
-    /* Call .initialize as the last thing we do as part of new() */
-    //console.log(1, _lite.test)
-    _lite.initialize.bind(_lite)();
-    return _lite;
+    /* onContentBound
+        Overridable. Is called after content is appended to the container
+    */
+    onContentBound() { }
+
+    #getContainer() { 
+        let element = this.container;
+        if(typeof(element) == 'string') {
+            element = document.getElementById(element);
+            if(!element)
+                element = document.querySelector(element);
+        }
+
+        return element instanceof HTMLElement
+            ? element
+            : this.container;
+    }
+
+    #appendContent() { 
+        while(this.container.firstChild)
+            this.container.removeChild(this.container.firstChild);
+        this.container.insertAdjacentHTML('afterbegin', this.content);
+    }
 }
-
-export let lite = new Lite();
