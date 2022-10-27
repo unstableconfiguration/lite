6f357f6a079d28b092d284b119481c5b0df44451
oldmanmuffins <mluper@protonmail.com>
2022-10-27 09:19:11 -0400

    router class rework

--- app/router.js
+++ app/router.js
@@ -1,52 +1,32 @@
-
+/* 
+*/
 export class Router {
-    // regex-value collection
-    //
+    /* { pattern : RegExp, value : any } */
     routes = [];
 
     constructor(options = {}) {
-
+        if(options.onHashChange) this.onHashChange = options.onHashChange;
+        window.onhashchange = this.#onHashChange;
     }
 
-    // This will need to be reworked for class syntax. 
-    onHashChange() { }
+    /* onHashChange - user supplied event
+        When locatino.hash is changed, is called with first matching route value. */
+    onHashChange(value) { }
 
-    
-}
-
-
-
-export let Router = function(options = {}){ 
-    let router = this;
-    router.routes = [ /* { pattern : /^xyz$/, value : any } */]
-
-    /* onHashChange: 
-        When hash is changed, will be called with the first matching route value
-    */
-    let onHashChange = function(value) { }
-    Object.defineProperty(router, 'onHashChange', {
-        get : function() { return onHashChange; },
-        set : function(val) { 
-            onHashChange = val;
-            window.onhashchange = router.__onHashChange;
-        }
-    });
-
-    router.__onHashChange = function() { 
+    #onHashChange() {
         let hash = location.hash
-            .slice(1) // Skip #
-            .split('?')[0]; // Ignore query 
-
-        let path = router.routes.find(route => {
-            return route.pattern.test(hash);
-        });        
+            .slice(1) // skip # symbol
+            .split('?')[0]; // ignore query
+            
+        let path = router.routes.find(route => 
+            route.pattern.test(hash));
         let value = path ? path.value : '404';
 
-        onHashChange(value);
+        this.onHashChange(value);
     }
-    router.onHashChange = options.onHashChange || onHashChange;
 
-    router.getSearchParams = function(search = location.search) { 
+    /* Parse url search into JSON object */
+    getSearchParams(search = location.search) {
         if(!search) {
             search = /\?.+$/.exec(location.hash);
             if(search) { search = search[0]; }
@@ -64,57 +44,47 @@ export let Router = function(options = {}){
         return objParams;
     }
 
-    router.addRoutes = function(routes) { 
-        if(!Array.isArray(routes)) { return; }
-        routes.forEach(route => router.addRoute(route));
+    addRoutes(routes) {
+        if(!Array.isArray(routes)) return;
+        routes.forEach(route => this.addRoute(route));
     }
 
-    /*
-        route:  { route : '', value : '' } 
-    */
-    router.addRoute = function(route) {
-        if(route.route instanceof RegExp) { 
+    addRoute(route) {
+        if(route.route instanceof RegExp) {
             route.pattern = route.route;
-            router.routes.push(route);
+            this.routes.push(route);
         }
-        if(typeof(route.route) !== 'string') { return; }
 
-        route.pattern = router.getPathRegex(route.route);
+        if(typeof(route.route) !== 'string') return; 
 
-        router.routes.push(route);
-        return router.routes;
+        route.pattern = this.#getPathRegex(route.route);
+
+        this.routes.push(route);
+        return this.routes;
     }
 
-    const escapeSpecialChars = function(path) {
-        ['(', ')', '/']
-            .forEach(specialChar => {
-                path = path.replace(specialChar, '\\' + specialChar);
-            });
-        return path;
-    }
-
-    /* Replaces syntax {wildcard} with .+ regex wildcard
-        /route/{wildcard} -> /route/.+
-    */
-    const escapeWildCard = function(path) { 
-        return path.replace(/{.+}/, '.+');
-    }
-
-    router.getPathRegex = function(path) {
-        path = escapeWildCard(path);
-        path = escapeSpecialChars(path);
+    #getPathRegex(path) {
+        path = this.#escapeWildCard(path);
+        path = this.#escapeSpecialChars(path);
 
         let pattern = new RegExp('^' + path + '$');
         return pattern;
+
     }
-
-    return router;
-
+    
+    /* Replaces syntax {wildcard} with .+ regex wildcard
+        /route/{wildcard} -> /route/.+
+    */
+    #escapeWildCard(path) {
+        return path.replace(/{.+}/g, '.+');
+    }
+    
+    /* Escapes regex special characters when building pattern.*/
+    #escapeSpecialChars(path) {
+        ['(', ')', '/']
+        .forEach(specialChar => {
+            path = path.replace(specialChar, '\\' + specialChar);
+        });
+        return path;
+    }
 }
-
-export let router = new Router();
-
-
-
-
-
